801eb3637c8647435b985b3ac142afb569c46936
tetsaiga86 <tetsaiga86@gmail.com>
2016-09-01 19:05:33 -0700

    problem 67 using node

---
+++ js/67/67.js
@@ -0,0 +1,20 @@
+var a = [];
+const readline = require('readline');
+const fs = require('fs');
+const rl = readline.createInterface({
+  input: fs.createReadStream('triangle.txt')
+});
+rl.on('line', function (line) {
+  a.push(line.split(' '))
+});
+
+rl.on('close', function (){
+  var lines = a.length;
+
+  for (var i = lines - 2; i >= 0; i--) {
+    for (var j = 0; j <= i; j++) {
+      a[i][j] = parseInt(a[i][j])+Math.max(parseInt(a[i+1][j]), parseInt(a[i+1][j+1]));
+    }
+  }
+console.log(a[0]);
+});
